79fe484b66f0e56ca5cb370a839ebc129d0097b4
attis82 <farkasatesz.82@gmail.com>
2025-03-25 18:39:22 +0100

    Creates Active Screen

--- app/src/main/java/com/farkasatesz/mytodo/MainActivity.kt
+++ app/src/main/java/com/farkasatesz/mytodo/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.farkasatesz.mytodo.ui.screens.activeScreen.ActiveScreen
+import com.farkasatesz.mytodo.ui.screens.activeScreen.ActiveScreenViewModel
 import com.farkasatesz.mytodo.ui.theme.MyTodoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,30 +16,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel : ActiveScreenViewModel by viewModels()
             MyTodoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ActiveScreen(viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyTodoTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/farkasatesz/mytodo/ui/components/TodoComponents.kt
+++ app/src/main/java/com/farkasatesz/mytodo/ui/components/TodoComponents.kt
@@ -1,24 +1,37 @@
 package com.farkasatesz.mytodo.ui.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -93,12 +106,12 @@ fun TodoInput(
 
 @Composable
 fun TodoDialog (
-    todo: Todo?,
     viewModel: ActiveScreenViewModel
 ) {
     val title by viewModel.title.collectAsStateWithLifecycle()
     val description by viewModel.description.collectAsStateWithLifecycle()
     val deadline by viewModel.deadline.collectAsStateWithLifecycle()
+    val selectedTodo by viewModel.selectedTodo.collectAsStateWithLifecycle()
 
     Dialog(
         onDismissRequest = { viewModel.hideDialog() }
@@ -159,16 +172,102 @@ fun TodoDialog (
                         }
                     ) {
                         val todo = Todo(
-                            todoId = todo?.todoId,
+                            todoId = selectedTodo?.todoId,
                             title = title,
                             description = description,
                             deadline = deadline
                         )
                         viewModel.upsertTodo(todo)
+                        viewModel.hideDialog()
+                        viewModel.reset()
                     }
                 }
             }
         }
     }
+}
 
+@Composable
+fun TodoCard(
+    todo: Todo,
+    getDeadline: () -> String,
+    onEdit: () -> Unit,
+    onDelete: () -> Unit,
+    setToCompleted: () -> Unit
+) {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(horizontal = 5.dp)
+        ,
+        border = BorderStroke(width = 1.dp, color = Color.Black),
+        shape = RoundedCornerShape(corner = CornerSize(10.dp))
+    ) {
+        Row(
+            Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                TodoText(
+                    text = todo.title,
+                    fontSize = 20,
+                    fontWeight = FontWeight.Bold
+                )
+                TodoText(
+                    text = todo.description
+                )
+                TodoText(
+                    text = getDeadline()
+                )
+            }
+            ActionIcons(
+                onEdit = onEdit,
+                onDelete = onDelete
+            ) {
+                setToCompleted()
+            }
+        }
+
+
+    }
+}
+
+@Composable
+private fun ActionIcons(
+    onEdit: () -> Unit,
+    onDelete: () -> Unit,
+    setToCompleted: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxHeight(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        TodoButton(
+            buttonText = {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+            }
+        ) {
+            onEdit()
+        }
+        TodoButton(
+            buttonText = {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+            }
+        ) {
+            onDelete()
+        }
+        TodoButton(
+            buttonText = {
+                Icon(imageVector = Icons.Default.Done, contentDescription = "Set To completed")
+            }
+        ) {
+            setToCompleted()
+        }
+    }
 }
